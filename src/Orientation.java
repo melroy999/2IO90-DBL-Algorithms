16fdadc95ff5df3162b525269fb32d7153a87b9e
Stephan Oostveen <s.p.o.oostveen@student.tue.nl>
2014-12-09 12:46:07 +0100

    Made project

---
+++ src/Orientation.java
@@ -0,0 +1,8 @@
+/*
+ * 2IO90 DBL Algorithms
+ * 14-11-2014 Stephan Oostveen
+ * This enumeration represents one of the four orientations possible.
+ */
+public enum Orientation {
+	NW, NE, SW, SE;
+}
