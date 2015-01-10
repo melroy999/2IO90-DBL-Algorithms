77fd254db0f870247d62b772aa693e90b7095452
s121318 <s121318@S121318.campus.tue.nl>
2015-01-11 00:40:03 +0100

    index of bound fixed

--- src/MapLabeler.java
+++ src/MapLabeler.java
@@ -3,9 +3,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
